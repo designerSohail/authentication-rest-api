ab405abdd269da6b48019dcc33c2bf8667b033f4
designerSohail <designersohail786@gmail.com>
2018-02-17 20:45:54 +0545

    GET /user/me to get details about user and authentication middleware added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true}).then(
+  () => {
+
+  }, err => {
+    console.log(err.message)
+  }
+)
 
 module.exports = {mongoose};
---
+++ server/middleware/authenticate.js
@@ -0,0 +1,15 @@
+var {User} = require('./../models/user');
+var authenticate = (req, res, next) => {
+  var token = req.header('x-auth')
+  User.findByToken(token).then((user) => {
+    if (!user) {
+      return Prmise.reject()
+    }
+    req.user = user
+    req.token = token
+    next()
+  }).catch(e => {
+    res.send(401).send('Authentication Error' + e)
+  })
+}
+module.exports = {authenticate}
