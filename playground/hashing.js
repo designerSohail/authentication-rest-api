e44b9e27861fbdfad085bfcd474ad08ae0e2f827
designerSohail <designersohail786@gmail.com>
2018-02-18 17:14:40 +0545

    Password hashing added before saving to db

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,19 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken')
-var data = {
-  id: 30
-}
-var token = jwt.sign(data, 'xyz')
-var decoded = jwt.verify(token, 'xyz')
-console.log(token)
-console.log(decoded)
+const bcrypt = require('bcryptjs')
+var pass = '12345jkjjlj!#'
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(pass, salt, (err, hash) => {
+    console.log(hash)
+  })
+})
+// var data = {
+//   id: 30
+// }
+// var token = jwt.sign(data, 'xyz')
+// var decoded = jwt.verify(token, 'xyz')
+// console.log(token)
+// console.log(decoded)
 // var message = 'Hello world'
 // var hash = SHA256(message).toString()
 // console.log(hash)
